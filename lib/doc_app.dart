a25b457415427109951398653e2589bb0081d526
engze <zeinab.srr@hotmail.com>
2024-09-01 04:15:40 +0300

    added firebase added fastlane bumbed first 101 and 102

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_project/core/routing/app_router.dart';
-import 'package:flutter_complete_project/core/routing/routes.dart';
 import 'package:flutter_complete_project/core/theming/colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routing/routes.dart';
 
 class DocApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -11,18 +12,17 @@ class DocApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      child: MaterialApp(
-        title: 'Doc App',
-        theme: ThemeData(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        child: MaterialApp(
+          title: 'Doc App',
+          theme: ThemeData(
             primaryColor: ColorsManager.mainBlue,
-        scaffoldBackgroundColor: Colors.white
-        ),
-        debugShowCheckedModeBanner: false,
-        initialRoute: Routes.onBoardingScreen,
-        onGenerateRoute: appRouter.generateRoute,
-      ),
-    );
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          debugShowCheckedModeBanner: false,
+          initialRoute:  Routes.onBoardingScreen,
+          onGenerateRoute: appRouter.generateRoute,
+        ));
   }
 }
---
+++ lib/main_production.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/di/dependency_injection.dart';
+import 'package:flutter_complete_project/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routing/app_router.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupGetIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
+
